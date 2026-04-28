d1a58bfbdccc755c1c8fed5202afaa2a4de9ad98
Jun Wang <richard.junwang@gmail.com>
2026-04-28 16:25:34 -0700

    add jun scripts

---
+++ examples/fully_async/run-qwen3-4b-fully_async_jun.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+# for rerun the task
+pkill -9 sglang
+sleep 3
+ray stop --force
+pkill -9 ray
+#pkill -9 python
+sleep 3
+pkill -9 ray
+#pkill -9 python
+
+set -ex
+
+# will prevent ray from buffering stdout/stderr
+export PYTHONBUFFERED=16
+
+NVLINK_COUNT=$(nvidia-smi topo -m 2>/dev/null | grep -o 'NV[0-9][0-9]*' | wc -l)
+if [ "$NVLINK_COUNT" -gt 0 ]; then
+    HAS_NVLINK=1
+else
+    HAS_NVLINK=0
+fi
+echo "HAS_NVLINK: $HAS_NVLINK (detected $NVLINK_COUNT NVLink references)"
+
+SRC_DIR=/opt/tiger/miles
+DATA_DIR=/opt/tiger/dapo-math-17k
+MEGATRON_PATH=/opt/tiger/Megatron-LM
+FULLY_ASYNC_DIR=${SRC_DIR}/examples/fully_async/
+MODEL_DIR=/opt/tiger/models/qwen3_4b
+MODEL_DIR_DIST=${MODEL_DIR}_torch_dist
+NUM_GPUS=4
+
+# download model and data
+hf download Qwen/Qwen3-4B --local-dir ${MODEL_DIR}
+hf download --repo-type dataset zhuzilin/dapo-math-17k --local-dir ${DATA_DIR}
+
+#SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "${SRC_DIR}/scripts/models/qwen3-4B.sh"
+
+#PYTHONPATH=${MEGATRON_PATH}:${SRC_DIR} python ${SRC_DIR}/tools/convert_hf_to_torch_dist.py \
+#    ${MODEL_ARGS[@]} \
+#    --hf-checkpoint ${MODEL_DIR} \
+#    --save ${MODEL_DIR_DIST}
+#
+#echo "Model exporting is done!!"
+
+
+CKPT_ARGS=(
+   --hf-checkpoint ${MODEL_DIR}
+   --ref-load ${MODEL_DIR_DIST}
+   --load ${MODEL_DIR}_miles
+   --save ${MODEL_DIR}_miles/
+   --save-interval 20
+)
+
+
+PROMPT_SET=${DATA_DIR}/dapo-math-17k.jsonl
+
+ROLLOUT_ARGS=(
+   --rollout-function-path fully_async_rollout.generate_rollout_fully_async
+   --prompt-data ${PROMPT_SET}
+   --input-key prompt
+   --label-key label
+   --apply-chat-template
+   --rollout-shuffle
+
+   --rm-type dapo
+   --reward-key score
+
+   --num-rollout 3000
+   --rollout-batch-size 32
+   --n-samples-per-prompt 8
+   --rollout-max-response-len 8192
+   --rollout-temperature 1
+
+   --global-batch-size 256
+   --balance-data
+
+   # for staleness control
+   #--max-weight-staleness 2
+)
+
+PERF_ARGS=(
+   --tensor-model-parallel-size 2
+   --sequence-parallel
+   --pipeline-model-parallel-size 1
+   --context-parallel-size 1
+   --expert-model-parallel-size 1
+   --expert-tensor-parallel-size 1
+
+   --recompute-granularity full
+   --recompute-method uniform
+   --recompute-num-layers 1
+
+   # --micro-batch-size 1
+   --use-dynamic-batch-size
+   --max-tokens-per-gpu 9216
+)
+
+GRPO_ARGS=(
+   --advantage-estimator grpo
+   --use-kl-loss
+   --kl-loss-coef 0.00
+   --kl-loss-type low_var_kl
+   --entropy-coef 0.00
+   --eps-clip 0.2
+   --eps-clip-high 0.28
+
+   --use-tis
+)
+
+OPTIMIZER_ARGS=(
+   --optimizer adam
+   --lr 1e-6
+   --lr-decay-style constant
+   --weight-decay 0.1
+   --adam-beta1 0.9
+   --adam-beta2 0.98
+)
+
+SGLANG_ARGS=(
+   --rollout-num-gpus-per-engine 1
+)
+
+MISC_ARGS=(
+   # default dropout in megatron is 0.1
+   --attention-dropout 0.0
+   --hidden-dropout 0.0
+   # should be good for model performance
+   --accumulate-allreduce-grads-in-fp32
+   --attention-softmax-in-fp32
+   # need to comment this when using model with MLA
+   --attention-backend flash
+)
+
+# launch the master node of ray in container
+export MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+ray start --head --node-ip-address ${MASTER_ADDR} --num-gpus ${NUM_GPUS} --disable-usage-stats
+
+RUNTIME_ENV_JSON="{
+  \"env_vars\": {
+    \"PYTHONPATH\": \"${MEGATRON_PATH}:${FULLY_ASYNC_DIR}\",
+    \"CUDA_DEVICE_MAX_CONNECTIONS\": \"1\",
+    \"NCCL_NVLS_ENABLE\": \"${HAS_NVLINK}\"
+  }
+}"
+
+ray job submit --address="http://127.0.0.1:8265" \
+   --runtime-env-json="${RUNTIME_ENV_JSON}" \
+   -- python3 train_async.py \
+   --actor-num-nodes 1 \
+   --actor-num-gpus-per-node 2 \
+   --rollout-num-gpus 2 \
+   ${MODEL_ARGS[@]} \
+   ${CKPT_ARGS[@]} \
+   ${ROLLOUT_ARGS[@]} \
+   ${OPTIMIZER_ARGS[@]} \
+   ${GRPO_ARGS[@]} \
+   ${PERF_ARGS[@]} \
+   ${SGLANG_ARGS[@]} \
+   ${MISC_ARGS[@]} 2>&1 | tee -a log.txt
---
+++ examples/fully_async/setup.sh
@@ -0,0 +1,67 @@
+
+#git clone https://github.com/radixark/miles /opt/tiger/miles
+
+# install nccl
+sudo bash -c '
+export NCCL_VERSION="2.27.5-1"
+cd /usr/local/src && git clone -b v${NCCL_VERSION} https://github.com/NVIDIA/nccl && cd nccl && \
+make -j src.build CUDA_HOME=/usr/local/cuda \
+NVCC_GENCODE="-gencode=arch=compute_100,code=sm_100" && \
+make install
+'
+
+
+MEGATRON_REPO=radixark/Megatron-LM
+MEGATRON_BRANCH=miles-main
+MEGATRON_PATH=/opt/tiger/Megatron-LM
+SGLANG_PATH=/opt/tiger/sglang
+MOONCAKE_PATH=/opt/tiger/mooncake
+
+# install apex
+NVCC_APPEND_FLAGS="--threads 4" \
+pip3 -v install --disable-pip-version-check --no-cache-dir --no-build-isolation \
+--config-settings "--build-option=--cpp_ext --cuda_ext --parallel 8" git+https://github.com/NVIDIA/apex.git@10417aceddd7d5d05d7cbf7b0fc2daad1105f8b4 --user
+
+
+# install megatron-lm
+# git clone https://github.com/radixark/Megatron-LM ${MEGATRON_PATH}
+# cd $MEGATRON_PATH
+# pip install -e . --user
+
+git clone https://github.com/${MEGATRON_REPO}.git --recursive -b ${MEGATRON_BRANCH} ${MEGATRON_PATH} && \
+    cd Megatron-LM && pip install -e . --user
+
+# cuda 13.1
+pip install nvidia-mathdx==25.6.0 --user && \
+pip -v install --no-build-isolation "transformer_engine[core_cu13,pytorch]==2.10.0" --user
+
+
+# install sglang
+git clone -b sglang-miles https://github.com/sgl-project/sglang ${SGLANG_PATH} && cd ${SGLANG_PATH} && pip3 install -e "python[all]" --no-deps --user
+
+# install mooncake
+#pip3 install mooncake-transfer-engine-cuda13 --user
+git clone --recurse-submodules https://github.com/kvcache-ai/Mooncake.git ${MOONCAKE_PATH}
+cd ${MOONCAKE_PATH}
+sudo bash ./dependencies.sh
+mkdir build
+cd build
+cmake ..
+make -j
+sudo make install
+
+
+#pip3 install -U "ray[all]"
+pip3 install -U "ray[default]" --user
+pip3 install typer httpx pylatexenc --user
+
+pip3 install sglang-router>=0.2.3 --user
+pip3 SGL_KERNEL_VERSION=0.3.17.post2 && \
+    python3 -m pip install https://github.com/sgl-project/whl/releases/download/v${SGL_KERNEL_VERSION}/sgl_kernel-${SGL_KERNEL_VERSION}+cu130-cp310-abi3-manylinux2014_$(uname -m).whl --force-reinstall --no-deps --user
+pip3 install git+https://github.com/ISEEKYAN/mbridge.git@89eb10887887bc74853f89a4de258c0702932a1c --no-deps --user
+
+pip3 install "protobuf<=3.20.3" --user
+pip3 install "numpy<2" --user
+
+# flash-attn
+MAX_JOBS=64 pip3 -v install flash-attn==2.7.4.post1 --no-build-isolation --user
